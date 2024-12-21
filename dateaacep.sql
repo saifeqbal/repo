7312252a8624519dae658d88001a148d4f84a15b
saifeqbal <39620668+saifeqbal@users.noreply.github.com>
2024-12-21 18:45:52 +0530

    Update

---
+++ dateaacep.sql
@@ -0,0 +1,8 @@
+create table new-viewsql ;
+view v_sql; 
+create database v_sql ;
+create table view_fom;{
+insert column[ name1 , varchar [varchar(89) , "name1 "];
+insert column[email_id, varchar[varchar(88), "email_id" ] ;
+insert column [mobile_number", varchar(89) ];
+}
